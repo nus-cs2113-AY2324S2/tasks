46ba27ddc314b7f742bba97ac06eb8da5dc45069
LuoYu-uwu <E0968957@u.nus.edu>
2024-02-23 17:17:39 +0800

    Implement stream in the code

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -35,6 +35,12 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+        int count = (int) tasks.stream().filter((t) -> t instanceof Deadline).count();
+
+        return count;
+    }
+
     public static void printAllData(ArrayList<Task> tasksData) {
         System.out.println("Printing data using iteration...");
         for (Task t : tasksData) {
@@ -46,12 +52,19 @@ public class Main {
         System.out.println("Printing data using stream...");
         tasks.stream().forEach(System.out::println);
     }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
+        System.out.println("Printing deadlines using iteration...");
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing deadlines using stream...");
+        tasks.stream().filter((t) -> t instanceof Deadline).forEach(System.out::println);
     }
 
 }
