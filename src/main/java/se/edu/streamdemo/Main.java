a9760d9f0f64064965591f38eb23632c08c9c824
runxinghuan <rxinghuan@gmail.com>
2024-02-23 17:17:31 +0800

    Implement print data using streams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -40,6 +40,12 @@ public class Main {
         }
     }
 
+    public static void printAllDataUsingStreams(ArrayList<Task> tasks) {
+        System.out.println();
+
+        tasks.stream()
+                .forEach(System.out::println);
+    }
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
